1bd8ac90d9dd092c46618c1e410a01ddaec2cdef
Fabio Lucas Marconi <fabiolukascj@gmail.com>
2022-10-18 23:57:33 -0400

    Tratando error docker

--- 03_continuando/02_bd/src/database/index.ts
+++ 03_continuando/02_bd/src/database/index.ts
@@ -1,3 +1,21 @@
-import { createConnection } from 'typeorm'
+// import { createConnection } from 'typeorm'
 
-createConnection();
+// createConnection();
+import { DataSource } from "typeorm"
+
+const AppDataSource = new DataSource({
+    "type": "postgres",
+    "host": "localhost",
+    "port": 54320,
+    "username": "docker",
+    "password": "rentx",
+    "database": "rentx",
+})
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
